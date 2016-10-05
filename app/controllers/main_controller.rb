9131929acd8ac690fff0b99b52f480c9ac54402f
Nick-Brennan <nick_p_b@yahoo.com>
2016-10-04 17:56:15 -0700

    prepped for building out solution

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,54 +8,28 @@ class MainController < ApplicationController
 
   def listen
 
-    response.headers['realtimeapi_code'] = params['realtimeapi_code']
-    # values_arr = params['entries'].gsub("\\", "")
-    # values_arr = params['entries'].gsub("\{", "")
-    # values_arr = params['entries'].split(":")
-    # p values_arr
-    if(params['entries'] != nil)
-      entries = JSON.parse(params['entries'])
-      p entries
-      c4 = false
-      c5 = false
+    email = #GET EMAIL ADDRESS
+    news = #GET NEWSLETTER OPT_IN STATUS
+    
+    myclient = ET_Client.new auth
+    p myclient
 
-      if(entries[0]['custom_4'] == "Yes")
-        c4 = true
-      end
+    dataextensionrow = FuelSDK::DataExtension::Row.new
+    dataextensionrow.authStub = myclient
+    dataextensionrow.Name = 'VBA_Website_Referral_List'
+    dataextensionrow.props = {"EmailAddress" => email,
+                              "Newsletter" => news}
+    results = dataextensionrow.post
 
-      if(entries[0]['custom_5'] == "Yes")
-        c5 = true
-      end
-
-      myclient = ET_Client.new auth
-      p myclient
-
-      dataextensionrow = FuelSDK::DataExtension::Row.new
-      dataextensionrow.authStub = myclient
-      dataextensionrow.Name = 'WOOBOX_API'
-      dataextensionrow.props = {"EmailAddress" => entries[0]['email'],
-                                "Signup_Time" => entries[0]['createdate'],
-                                "Age" => entries[0]['age'],
-                                "City" => entries[0]['city'],
-                                "Class" => entries[0]['class'],
-                                "Custom4" => c4,
-                                "Custom5" => c5,
-                                "FName" => entries[0]['first_name'],
-                                "LName" => entries[0]['last_name'],
-                                "Occupation" => entries[0]['occupation'],
-                                "OtherProfession" => entries[0]['other_profession'],
-                                "IP_Address" => entries[0]['ipaddress']}
-      results = dataextensionrow.post
-
-      p results.results
-    end
+    p results.results
+    
   end
 
   def auth
       {
         'client' => {
-          'id' => "7b0dwqmgp5sebmnf5fbvedwr",
-          'secret' => "dojfvyOTi28i48kyhWOCotv1"
+          'id' => [CLIENT_ID],
+          'secret' => [CLIENT_SECRET]
         }
       }
     end
